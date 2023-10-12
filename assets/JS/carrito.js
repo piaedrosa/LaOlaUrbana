e116c9a88dcc5126b6592d14fba9a2f0b7c1193c
Pia Edrosa <piaedrosa@gmail.com>
2023-10-12 10:01:40 -0300

    cambio en css

--- assets/JS/carrito.js
+++ assets/JS/carrito.js
@@ -11,8 +11,8 @@ const contenedorTotal = document.querySelector("#total");
 const botonComprar = document.querySelector("#carrito-acciones-comprar");
 
 
-function cargarProductosCarrito() {
-    if (productosEnCarrito && productosEnCarrito.length > 0) {
+//function cargarProductosCarrito() {
+/*    if (productosEnCarrito && productosEnCarrito.length > 0) {
 
         contenedorCarritoVacio.classList.add("disabled");
         contenedorCarritoProductos.classList.remove("disabled");
